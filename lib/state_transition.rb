c9b89fb5ab10e30a64bfc63c1128c2ef438c0d50
Matt Lightner <mlightner@gmail.com>
2009-02-16 09:55:50 -0500

    Support :failure_message on events and add to errors array on failure.

--- lib/state_transition.rb
+++ lib/state_transition.rb
@@ -4,18 +4,25 @@ module AASM
       attr_reader :from, :to, :opts
 
       def initialize(opts)
-        @from, @to, @guard, @on_transition = opts[:from], opts[:to], opts[:guard], opts[:on_transition]
+        @from, @to, @guard, @on_transition, @failure_message = opts[:from], opts[:to], opts[:guard], opts[:on_transition], opts[:failure_message]
         @opts = opts
       end
 
       def perform(obj)
-        case @guard
+        result = case @guard
         when Symbol, String
           obj.send(@guard)
         when Proc
           @guard.call(obj)
         else
           true
+        end
+
+        if result
+          true
+        else
+          obj.errors.send(:add, obj.class.aasm_column.to_sym, @failure_message)
+          false
         end
       end
       
