a412c7ae961b7c38e225a470b187c6dd741ad96f
jeanmartin <jan@schwenzien.info>
2009-06-07 14:57:29 -0700

    more generic guard failure handling (using I18n)

--- lib/state_transition.rb
+++ lib/state_transition.rb
@@ -25,10 +25,10 @@ module AASM
           end
 
           # Catch generic false return with no exception raised
-          raise(GuardFailure, "the guard process did not pass") unless guard_result
+          raise(GuardFailure, I18n.t('activerecord.errors.messages.guard_fail', :default => 'guard condition(s) failed')) unless guard_result
 
         rescue GuardFailure => e
-          obj.errors.send(:add, obj.class.aasm_column.to_sym, "is invalid because #{e.message}")
+          obj.errors.send(:add, obj.class.aasm_column.to_sym, e.message)
           false
         else
           true
